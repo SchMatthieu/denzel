0d1aa76c2a51b817842f6a72bd162d48074a27e6
matts <matthieu.schoeser@devinci.fr>
2019-03-22 19:06:26 +0100

    GraphQL done

--- api.js
+++ api.js
@@ -1,8 +1,12 @@
 const Express = require('express');
 const BodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
-const ObjectId = require('mongodb').ObjectID;
+const cors = require('cors');
 const imdb = require('./src/imdb.js');
+
+const graphqlHTTP = require('express-graphql');
+const { GraphQLSchema } = require('graphql');
+const { GraphQLObjectType, GraphQLString, GraphQLNonNull, GraphQLList } = require('graphql');
 
 const DENZEL_IMDB_ID = 'nm0000243';
 const CONNECTION_URL = 'mongodb+srv://user_1:Schoeser1998@webappdenzel-wcs49.mongodb.net/test?retryWrites=true';
@@ -10,6 +14,7 @@ const DATABASE_NAME = 'WebappDenzel';
 
 var app = Express();
 
+app.use(cors());
 app.use(BodyParser.json());
 app.use(BodyParser.urlencoded({ extended: true }));
 
@@ -20,7 +25,6 @@ var collection;
 
 app.get('/movies/populate', async (req, res) => {
 	var movies = await imdb(DENZEL_IMDB_ID);
-	var counter = 0;
 	movies.forEach((movie) => {
 		var nbDocs = 0;
 		collection.find({ id: `${movie.id}` }).toArray((err, docs) => {
@@ -29,19 +33,20 @@ app.get('/movies/populate', async (req, res) => {
 			}
 			nbDocs = docs.length;
 			if (nbDocs > 0) {
-				console.log('Movie ' + movie.id + ' already in the DB');
+				console.log('Movie ' + movie.id + ' already in the DB..');
 			} else {
 				counter = counter + 1;
 				collection.insertOne(movie, (err) => {
 					if (err) {
 						console.log(err);
 					}
-					console.log('Movie ' + movie.id + ' added to the DB..');
+					console.log('Movie ' + movie.id + ' added to the DB !');
 				});
 			}
 		});
 	});
-	res.send('DB updated with ' + counter + ' movies !');
+	var counter = await collection.countDocuments();
+	res.send('Movies in the DB: ' + counter);
 });
 
 app.get('/movies/delete', async (req, res) => {
@@ -101,6 +106,114 @@ app.post('/movies/:id', (req, res) => {
 });
 
 /*---------- ENDPOINTS ----------*/
+
+/*---------- GRAPHQL ----------*/
+
+const { movieType } = require('./schema.js');
+
+const queryType = new GraphQLObjectType({
+	name: 'Query',
+	fields: {
+		moviesPopulate: {
+			type: GraphQLString,
+			resolve: async function() {
+				var movies = await imdb(DENZEL_IMDB_ID);
+				movies.forEach((movie) => {
+					var nbDocs = 0;
+					collection.find({ id: `${movie.id}` }).toArray((err, docs) => {
+						if (err) {
+							return response.status(500).send(err);
+						}
+						nbDocs = docs.length;
+						if (nbDocs > 0) {
+							console.log('Movie ' + movie.id + ' already in the DB..');
+						} else {
+							counter = counter + 1;
+							collection.insertOne(movie, (err) => {
+								if (err) {
+									console.log(err);
+								}
+								console.log('Movie ' + movie.id + ' added to the DB !');
+							});
+						}
+					});
+				});
+				var counter = await collection.countDocuments();
+				return 'Movies in the DB : ' + counter;
+			}
+		},
+		moviesDelete: {
+			type: GraphQLString,
+			resolve: function() {
+				collection.deleteMany({});
+				return 'DB cleared !';
+			}
+		},
+		moviesSearchByID: {
+			type: movieType,
+			args: {
+				id: { type: GraphQLString }
+			},
+			resolve: async function(source, args) {
+				return await collection.findOne({ id: args.id });
+			}
+		},
+		moviesSearchRandom: {
+			type: movieType,
+			resolve: async function() {
+				var movies = await collection.find({ metascore: { $gte: 70 } }).toArray();
+				let random = Math.floor(Math.random() * Math.floor(movies.length));
+				return movies[random];
+			}
+		},
+		moviesSearch: {
+			type: new GraphQLList(movieType),
+			args: {
+				limit: { type: GraphQLString },
+				metascore: { type: GraphQLString }
+			},
+			resolve: async function(source, args) {
+				var meta = 0,
+				limit = 5;
+				meta = Number(args.metascore);
+				if (meta > 100) meta = 100;
+				if (Number(args.limit) <= 5) limit = Number(args.limit);
+				movies = await collection.find({ metascore: { $gte: meta } }).sort({ metascore: -1 }).toArray();
+				var moviesSearched = [];
+				for (let i = 0; i < limit; i++) {
+					if (movies[i] != null) {
+						moviesSearched.push(movies[i]);
+					}
+				}
+				return moviesSearched;
+			}
+		},
+		moviesUpdate: {
+			type: movieType,
+			args: {
+				date: { type: GraphQLString },
+				review: { type: GraphQLString },
+				id: { type: GraphQLString }
+			},
+			resolve: async function(source, args) {
+				collection.updateOne({ id: args.id }, { $set: { date: args.date, review: args.review } });
+				return await collection.findOne({ id: args.id });
+			}
+		}
+	}
+});
+
+const schema = new GraphQLSchema({ query: queryType });
+
+app.use(
+	'/graphql',
+	graphqlHTTP({
+		schema: schema,
+		graphiql: true
+	})
+);
+
+/*---------- GRAPHQL ----------*/
 
 app.listen(9292, () => {
 	MongoClient.connect(CONNECTION_URL, { useNewUrlParser: true }, (error, client) => {
---
+++ schema.js
@@ -0,0 +1,20 @@
+const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLFloat } = require('graphql');
+
+movieType = new GraphQLObjectType({
+	name: 'Movie',
+	fields: {
+		id: { type: GraphQLString },
+		link: { type: GraphQLString },
+		metascore: { type: GraphQLInt },
+		poster: { type: GraphQLString },
+		rating: { type: GraphQLFloat },
+		synopsis: { type: GraphQLString },
+		title: { type: GraphQLString },
+		votes: { type: GraphQLInt },
+		year: { type: GraphQLInt },
+		date: { type: GraphQLString },
+		review: { type: GraphQLString }
+	}
+});
+
+exports.movieType = movieType;
